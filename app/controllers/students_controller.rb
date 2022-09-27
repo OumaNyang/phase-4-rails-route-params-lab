cf7ebf759722a9d57019667598224435d8260d98
Milton Ouma <ouma.nyang1@gmail.com>
2022-09-27 16:04:28 +0300

    Completed

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,5 +4,13 @@ class StudentsController < ApplicationController
     students = Student.all
     render json: students
   end
-
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
+  def grades
+    student = Student.find(params[:grades])
+    render json: student
+  end
+  
 end
